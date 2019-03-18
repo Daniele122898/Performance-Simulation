a3d8fc77b63a66a3ee56fce85cb2c3a54caae0b7
Daniele122898 <d.palbanese@gmail.com>
2019-03-18 22:22:20 +0100

    saving some more stuff

--- PerformancePlaci/Data/Storage.cs
+++ PerformancePlaci/Data/Storage.cs
@@ -1,18 +1,42 @@
 using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace PerformancePlaci.data
 {
     public static class Storage
     {
+        static private JsonSerializer _jSerializer = new JsonSerializer();
+        
         public static List<Step> PerformanceList = new List<Step> ();
         public static double PerformanceIndexed = 100;
         public static List<double> IndexedHistory { get; set; } = new List<double>();
 
         static Storage()
         {
+            _jSerializer.Converters.Add(new JavaScriptDateTimeConverter());
+            _jSerializer.NullValueHandling = NullValueHandling.Ignore;
+        }
+
+        public static void Reset()
+        {
+            PerformanceIndexed = 100;
+            PerformanceList.Clear();
+            IndexedHistory.Clear();
             IndexedHistory.Add(100.0);    
         }
-        
+
+        public static void Save(string path, int amount)
+        {
+            using (StreamWriter sw = File.CreateText($"{path}/performanceHistory_{amount}.json"))
+            {
+                using (JsonWriter writer = new JsonTextWriter(sw))
+                {
+                    _jSerializer.Serialize(writer, PerformanceList);
+                }
+            }
+        }
     }
 
     public class Step
--- PerformancePlaci/Program.cs
+++ PerformancePlaci/Program.cs
@@ -10,32 +10,44 @@ namespace PerformancePlaci
     {
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("How many Indexes do you want to choose each iteration: ");
-            if (!int.TryParse(Console.ReadLine().Trim(), out int indexCount))
-            {
-                Console.WriteLine("You stupid. Enter number pls");
-                System.Environment.Exit(-1);
-            }
-            
+
             (List<string> header, List<Line> lines) = CsvParser.Parse("./Data/data.csv");
 
-            int endIndex = 131;
-            // calculate performances
             while (true)
             {
-                var performances = Performance.Calc(lines, endIndex);
-                if (performances == null) break;
+                Storage.Reset();
+                Console.WriteLine("How many Indexes do you want to choose each iteration: ");
+                if (!int.TryParse(Console.ReadLine().Trim(), out int indexCount))
+                {
+                    Console.WriteLine("You stupid. Enter number pls");
+                    System.Environment.Exit(-1);
+                }
                 
-                Performance.CalcPerformance(indexCount, endIndex+1, lines, header, performances);
-
-                endIndex += 22;
+    
+                int endIndex = 131;
+                // calculate performances
+                while (true)
+                {
+                    var performances = Performance.Calc(lines, endIndex);
+                    if (performances == null) break;
+                    
+                    Performance.CalcPerformance(indexCount, endIndex+1, lines, header, performances);
+    
+                    endIndex += 22;
+                }
+                
+                CsvParser.WriteFile("./Data/", indexCount);
+                
+                Storage.Save("./Data", indexCount);
+                
+                Console.WriteLine($"Total Indexed Performance: {Storage.PerformanceIndexed}");
+                
+                if (Console.ReadLine()?.Equals("exit", StringComparison.OrdinalIgnoreCase) ?? false)
+                {
+                    Environment.Exit(0);
+                }
+                
             }
-            
-            CsvParser.WriteFile("./Data/", indexCount);
-            
-            Console.WriteLine($"Total Indexed Performance: {Storage.PerformanceIndexed}");
-            Console.Read();
 
         }
     }
